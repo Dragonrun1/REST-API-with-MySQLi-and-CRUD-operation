63cf015dd55dc7d546e0390ea4ccfee02e0d9226
Michael Cummings <mgcummings@yahoo.com>
2016-04-23 07:48:54 -0700

    Reformat SQL file.

--- bharatcode.sql
+++ bharatcode.sql
@@ -7,13 +7,11 @@
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
 --
@@ -27,16 +25,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `bh_user_master` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(40) NOT NULL,
-  `lastname` varchar(40) NOT NULL,
-  `email` varchar(40) NOT NULL,
-  `password` text NOT NULL,
-  `register_date` datetime NOT NULL,
-  `register_ipaddress` varchar(40) NOT NULL,
-  `user_type` int(1) NOT NULL COMMENT '0:user,1: admin',
-  `active_status` int(1) NOT NULL COMMENT '0:inactive, 1:active',
-  `verify_code` varchar(100) NOT NULL,
-  `verify_status` int(1) NOT NULL COMMENT '0:Pending, 1:Verified',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+    `user_id`            INT(11)      NOT NULL AUTO_INCREMENT,
+    `firstname`          VARCHAR(40)  NOT NULL,
+    `lastname`           VARCHAR(40)  NOT NULL,
+    `email`              VARCHAR(40)  NOT NULL,
+    `password`           TEXT         NOT NULL,
+    `register_date`      DATETIME     NOT NULL,
+    `register_ipaddress` VARCHAR(40)  NOT NULL,
+    `user_type`          INT(1)       NOT NULL
+    COMMENT '0:user,1: admin',
+    `active_status`      INT(1)       NOT NULL
+    COMMENT '0:inactive, 1:active',
+    `verify_code`        VARCHAR(100) NOT NULL,
+    `verify_status`      INT(1)       NOT NULL
+    COMMENT '0:Pending, 1:Verified',
+    PRIMARY KEY (`user_id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = latin1
+    AUTO_INCREMENT = 1;
